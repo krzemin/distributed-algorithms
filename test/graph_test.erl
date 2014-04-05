fdb6db54c6cf7ed270448704923f20c9b8bb6226
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-04-05 09:56:04 +0200

    some complex clique cases added

--- test/graph_test.erl
+++ test/graph_test.erl
@@ -10,6 +10,13 @@ clique_basic_test() ->
   ?assert(clique(2) == [{1,2}]).
 
 clique_complex_test() ->
-  ?assert(clique(3) == [{1,2},{1,3},{2,3}]).
+  C3 = [{1,2},{1,3},{2,3}],
+  ?assert(clique(3) == C3),
+  C4 = lists:append(C3, [{1,4},{2,4},{3,4}]),
+  ?assert(clique(4) == C4),
+  C5 = lists:append(C4, [{1,5},{2,5},{3,5},{4,5}]),
+  ?assert(clique(5) == C5),
+  C6 = lists:append(C5, [{1,6},{2,6},{3,6},{4,6},{5,6}]),
+  ?assert(clique(6) == C6).
 
 
