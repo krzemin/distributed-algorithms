aa0f4b6737e8bb1de82fb2e2f561ac6955843f54
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-04-15 18:27:39 +0200

    initializer broadcasting; flipbit & diffidx implementation & tests

--- src/cube_broadcast.erl
+++ src/cube_broadcast.erl
@@ -12,25 +12,42 @@
 %% API
 %%-export([]).
 
-init(K) ->
+init(K, N) ->
   receive
     Pids ->
       io:format(user, "initializing process ~p with pid list ~p~n", [K, Pids]),
-      p(K, Pids)
+      p(K, N, Pids)
   end.
 
-p(K, Pids) ->
+p(K, N, Pids) ->
   receive
     die ->
-      io:format(user, "process ~p received die message~n", [K])
+      io:format(user, "process ~p received die message~n", [K]) ;
+    {K1, M} ->
+      io:format(user, "process ~p received message ~p from ~p~n", [K, M, K1]),
+
+      p(K, N, Pids) ;
+    M ->
+      lists:foreach(fun(I) ->
+          K1 = flipbit(K, I),
+          Pid = lists:nth(K1, Pids),
+          Pid ! {K, M}
+        end,
+        lists:seq(0, N - 1)
+      ),
+      p(K, N, Pids)
   end.
 
+flipbit(K, I) ->
+  K bxor (1 bsl I).
 
+diffidx(K1, K2) ->
+  round(math:log(K1 bxor K2) / math:log(2)).
 
 
 setup(N) ->
   Ids = lists:seq(0, round(math:pow(2, N)) - 1),
-  Pids = lists:map(fun(K) -> spawn(fun() -> init(K) end) end, Ids),
+  Pids = lists:map(fun(K) -> spawn(fun() -> init(K, N) end) end, Ids),
   lists:foreach(fun(Pid) -> Pid ! Pids end, Pids),
   Pids.
 
--- test/cube_broadcast_test.erl
+++ test/cube_broadcast_test.erl
@@ -2,14 +2,35 @@
 -author("krzemin").
 
 -include_lib("eunit/include/eunit.hrl").
--import(cube_broadcast, [setup/1]).
-
-
+-import(cube_broadcast, [setup/1, flipbit/2, diffidx/2]).
 
 
 spawn_and_die_test() ->
-  io:format("hahahaha~n"),
-  Pids = cube_broadcast:setup(4),
+  Pids = setup(4),
   ?assert(16 == length(Pids)),
   lists:foreach(fun(Pid) -> Pid ! die end, Pids).
+
+broadcast_test() ->
+  Pids = setup(4),
+  ?assert(16 == length(Pids)),
+  [P0 | _] = Pids,
+  P0 ! secret_message.
+
+flipbit_test() ->
+  ?assert(1 == flipbit(0, 0)),
+  ?assert(0 == flipbit(1, 0)),
+  ?assert(3 == flipbit(2, 0)),
+  ?assert(2 == flipbit(3, 0)),
+  ?assert(0 == flipbit(2, 1)),
+  ?assert(2 == flipbit(0, 1)),
+  ?assert(6 == flipbit(4, 1)).
+
+diffidx_test() ->
+  ?assert(0 == diffidx(1, 0)),
+  ?assert(0 == diffidx(3, 2)),
+  ?assert(1 == diffidx(3, 1)),
+  ?assert(2 == diffidx(4, 0)),
+  ?assert(3 == diffidx(10, 2)),
+  ?assert(2 == diffidx(15, 11)).
+
 
