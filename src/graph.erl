fa2c1a791bf5ed8007a3d73758f2438b3a0a9a2a
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-04-15 19:33:29 +0200

    helper graph routines

--- src/graph.erl
+++ src/graph.erl
@@ -2,7 +2,7 @@
 -author("krzemin").
 
 %% API
--export([clique/1, map/2]).
+-export([clique/1, map/2, graph_size/1, to_neighbours_array/1]).
 
 -spec clique(N) -> [Edge] when
   N :: non_neg_integer(),
@@ -24,3 +24,22 @@ clique(N) ->
 
 map(F, G) ->
   lists:map(fun({V,U}) -> {F(V), F(U)} end, G).
+
+graph_size([]) -> 0 ;
+graph_size([{V1, V2} | Es]) ->
+  max(max(V1, V2), graph_size(Es)).
+
+to_neighbours_array(G) ->
+  N = graph_size(G),
+  to_neighbours_array(G, array:new(N+1, {default, []})).
+
+to_neighbours_array([], Arr) ->
+  array:map(fun(_, L) -> lists:sort(L) end, Arr) ;
+to_neighbours_array([{V1, V2}|Es], Arr) ->
+  V1List = array:get(V1, Arr),
+  V2List = array:get(V2, Arr),
+  Arr1 = array:set(V1, [V2|V1List], Arr),
+  Arr2 = array:set(V2, [V1|V2List], Arr1),
+  to_neighbours_array(Es, Arr2).
+
+
--- test/graph_test.erl
+++ test/graph_test.erl
@@ -2,7 +2,7 @@
 -author("krzemin").
 
 -include_lib("eunit/include/eunit.hrl").
--import(graph, [clique/1, map/2]).
+-import(graph, [clique/1, map/2, graph_size/1, to_neighbours_array/1]).
 
 clique_basic_test() ->
   ?assert(clique(0) == []),
@@ -24,3 +24,21 @@ map_test() ->
   C3Plus10 = map(fun(V) -> V + 10 end, C3),
   ?assert(C3Plus10 == [{11,12},{11,13},{12,13}]).
 
+graph_size_test() ->
+  ?assert(2 == graph_size(clique(2))),
+  ?assert(5 == graph_size(clique(5))),
+  ?assert(10 == graph_size(clique(10))),
+  ?assert(2 == graph_size([{1,2}])),
+  ?assert(3 == graph_size([{1,2}, {2,3}])),
+  ?assert(6 == graph_size([{1,2}, {4,3}, {6,1}, {2, 5}])).
+
+
+to_neighbours_array_test() ->
+  NA1 = to_neighbours_array(clique(5)),
+  ?assert([1,3,4,5] == array:get(2, NA1)),
+  NA2 = to_neighbours_array(clique(10)),
+  ?assert([1,2,3,4,5,6,7,8,10] == array:get(9, NA2)),
+  NA3 = to_neighbours_array([{1,2},{2,3},{3,4}]),
+  ?assert([1,3] == array:get(2, NA3)),
+  NA4 = to_neighbours_array([{1,4},{3,8},{6,20}]),
+  ?assert([] == array:get(17, NA4)).
