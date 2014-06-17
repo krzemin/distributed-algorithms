ad6eb133473283afbb023f06f9033725c50a3b1e
Piotr Krzemiński <pio.krzeminski@gmail.com>
2014-06-17 16:13:27 +0200

    broadcasting test on a cycle

--- test/graph_broadcast_test.erl
+++ test/graph_broadcast_test.erl
@@ -3,7 +3,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 -import(graph_broadcast, [setup/1]).
--import(graph, [clique/1]).
+-import(graph, [clique/1,cycle/1]).
 
 
 spawn_and_die_test() ->
@@ -11,8 +11,14 @@ spawn_and_die_test() ->
   ?assert(6 == length(IdsPids)),
   lists:foreach(fun({_, Pid}) -> Pid ! die end, IdsPids).
 
-broadcast_test() ->
+broadcast_clique_test() ->
   IdsPids = setup(clique(6)),
   ?assert(6 == length(IdsPids)),
+  [{_, P0}|_] = IdsPids,
+  P0 ! secret_message.
+
+broadcast_cycle_test() ->
+  IdsPids = setup(clique(8)),
+  ?assert(8 == length(IdsPids)),
   [{_, P0}|_] = IdsPids,
   P0 ! secret_message.
